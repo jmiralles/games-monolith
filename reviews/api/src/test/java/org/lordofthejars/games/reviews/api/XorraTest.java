82d4e35c7dd98e74664f0a49ac9a4d93c369c145
Christian Soler <solero93@gmail.com>
2019-03-28 20:11:17 +0100

    XorraTest

---
+++ reviews/api/src/test/java/org/lordofthejars/games/reviews/api/XorraTest.java
@@ -0,0 +1,11 @@
+package org.lordofthejars.games.reviews.api;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class XorraTest {
+  @Test
+  public void test_xorra() {
+    Assert.assertTrue(true);
+  }
+}
